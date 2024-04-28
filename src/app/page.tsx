1df1f47c6a73142a0170b7beccf4e03c8fffb9c7
Parkhomenko Egor <parkhomenko2049@gmail.com>
2024-04-28 16:33:25 +0600

    [Feat] Scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
-
 export default function HomePage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-    (Code in progress)
-    </main>
-  );
+    const mockUrls = [
+        'https://utfs.io/f/a246cec9-4085-4e25-ae4e-870f20a5a5bc-tg0fvs.1000x1000x1.png',
+        'https://utfs.io/f/035ba44b-8457-451d-9e86-a3c3ea799c29-mu01pe.1000x1000x1.jpg',
+        'https://utfs.io/f/9941f471-6bb3-4084-b0c3-e0fc1ca03505-xk2g87.1000x1000x1.png',
+        'https://utfs.io/f/f4f0fd14-b501-4156-b1db-c21b94fb6367-a2truw.png',
+    ];
+
+    const mockImages = mockUrls.map((url, index) => ({
+        id: index + 1,
+        url,
+    }));
+
+    return (
+        <main className=''>
+            <div className={'flex flex-wrap gap-4'}>
+                {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map(image => (
+                    <div key={image.id} className={'w-48 p-4'}>
+                        <img src={image.url} alt={'image'} />
+                    </div>
+                ))}
+            </div>
+        </main>
+    );
 }
