25c8dc1086c57c096cb2eb3ed113a0efbc40cbbb
Parkhomenko Egor <parkhomenko2049@gmail.com>
2024-04-29 22:10:47 +0600

    [Feat] Set up database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
-export default function HomePage() {
+import { db } from '~/server/db';
+
+export default async function HomePage() {
+    const posts = await db.query.posts.findMany();
+
+    console.log(posts);
+
     const mockUrls = [
         'https://utfs.io/f/a246cec9-4085-4e25-ae4e-870f20a5a5bc-tg0fvs.1000x1000x1.png',
         'https://utfs.io/f/035ba44b-8457-451d-9e86-a3c3ea799c29-mu01pe.1000x1000x1.jpg',
@@ -14,8 +20,11 @@ export default function HomePage() {
     return (
         <main className=''>
             <div className={'flex flex-wrap gap-4'}>
-                {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map(image => (
-                    <div key={image.id} className={'w-48 p-4'}>
+                {posts.map(post => (
+                    <div key={post.id}>{post.name}</div>
+                ))}
+                {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+                    <div key={image.id + '-' + index} className={'w-48 p-4'}>
                         <img src={image.url} alt={'image'} />
                     </div>
                 ))}
